cf44c5f14a222e12b8d2407e367ece03bd4cff58
Sree krishna <sreekrishnakaturi@gmail.com>
2022-05-07 17:41:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple website created using OMDB API to get all the stats and information about the movies and TV shows with a single click.
+ 
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const ejs = require('ejs');
+const axios = require('axios');
+require('dotenv').config()
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended : true}));
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) =>{
+    res.render('home');
+})
+
+app.post('/',(req,res) =>{
+    const year = req.body.year;
+    let url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${req.body.movie}`;
+    if(year){
+        url = url + `&y=${year}`;
+    }
+
+    axios.get(url)
+        .then((response) =>{
+            if(response.data.Response === 'True'){
+                res.render('movie', {
+                    movieTitle : response.data.Title, 
+                    moviePlot : response.data.Plot, 
+                    moviePoster : response.data.Poster === 'N/A'?'images/noposter.jpg':response.data.Poster,
+                    imdbRating : typeof response.data.Ratings[0] === 'undefined'? "-/-" :response.data.Ratings[0].Value ,
+                    rottenRating : typeof response.data.Ratings[1] === 'undefined'? "-/-" :response.data.Ratings[1].Value ,
+                    collection: typeof response.data.BoxOffice === 'undefined'? "-/-" :response.data.BoxOffice ,
+                    genre : response.data.Genre,
+                    actors: response.data.Actors,
+                    director : response.data.Director,
+                    language : response.data.Language,
+                    awards : response.data.Awards,
+                });
+            }else{
+                res.render("error");
+            }
+        })
+        .catch((err) =>{
+            res.render('error');
+        })
+})
+
+app.get('/about',(req,res) =>{
+    res.render('about');
+})
+
+app.listen(port , ()=> {
+    console.log(`Server started on port ${port}`); 
+});
