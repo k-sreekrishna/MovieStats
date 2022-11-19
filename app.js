e71beaa5b9cd50871d9b5b07f135a9ea60c4b4b7
sreekrishna <sreekrishnakaturi@gmail.com>
2022-11-19 18:05:12 +0530

    fix problem access /public folder in deployed app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
